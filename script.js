0e77b1ddce30967aec967fdf52ff22f4c1aa6a24
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-14 13:09:11 -0400

    Lay out structure of code

---
+++ script.js
@@ -0,0 +1,22 @@
+/* 
+PSEUDOCODE
+- convert to comments and delete later
+- 3 main sections: 
+    - js logic functions
+    - html calculator w/ display functions
+    - js logic implementation into html calculator
+
+
+***JS Logic Functions (steps 1-3)***
+
+step 1: basic math operations
+step 2: operation variables and function
+step 3: operator function
+
+***HTML Calculator w/ Display Functions (steps 4-5)***
+step 4: basic html calculator using dom
+step 5: display functions
+
+***JS Logic Implementation into HTML Calculator (step 6)****
+step 6: integrate js and html so the calculator works (user inputs an operator, calculator outputs the correct response)
+*/
