2bcc63e3e49edba41095deb01cfad664cc1e6adc
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-14 15:13:17 -0400

    Add pseudocode for asg steps 1-3

--- script.js
+++ script.js
@@ -20,3 +20,50 @@ step 5: display functions
 ***JS Logic Implementation into HTML Calculator (step 6)****
 step 6: integrate js and html so the calculator works (user inputs an operator, calculator outputs the correct response)
 */
+
+// JS Logic Functions (steps 1-3)
+// step 1
+
+/* Function add two numbers
+Pass In: two numbers
+Action: CALCULATE the sum of the numbers
+Pass Out: sum of the numbers
+ENDFUNC
+*/
+
+/* Function subtract two numbers
+Pass In: two numbers
+Action: CALCULATE the difference of the numbers
+Pass Out: difference of the numbers
+ENDFUNC
+*/
+
+/* Function multiply two numbers
+Pass In: two numbers
+Action: CALCULATE the product of the numbers
+Pass Out: product of the numbers
+ENDFUNC
+*/
+
+/* Function divide two numbers
+Pass In: two numbers
+Action: CALCULATE the quotient of the numbers
+Pass Out: quotient of the numbers
+ENDFUNC
+*/
+
+// step 2
+// INIT let variables to store an operand, an operator, and an operand
+
+// step 3
+/* Function perform an operation
+Pass In: two operands, one operator
+Action: 
+    CASE operator value OF
+    +: call addition function and pass operands
+    -: call subtraction function and pass operands
+    *: call multiplication function and pass operands
+    /: call addition function and pass operands
+    ENDCASE
+Pass Out: the result of the operation
+*/
