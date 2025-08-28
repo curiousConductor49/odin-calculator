cdf09c5b4439bf597848f3215d04a438ede3f57b
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-08-28 17:52:03 -0400

    Fix dividing by zero catch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Declare variables
+// declare variables to store DOM nodes
 
 const numberKeys = document.querySelectorAll(".number-key");
 const operatorKeys = document.querySelectorAll(".operator-key");
@@ -7,12 +7,13 @@ const equalSign = document.querySelector(".equal-sign");
 const clearBtn = document.querySelector(".clear-btn");
 const displayPanel = document.querySelector(".display");
 
+// declare variables for operands, operator, and result of operation
 let operand1 = [];
 let operand2 = [];
 let operator = null;
 let operationResult = null;
 
-// Declare functions
+// create functions for basic math operations
 
 function add(a, b) {
     return a + b;
@@ -31,18 +32,18 @@ function divide(a, b) {
 }
 
 function operate(num1, num2, operator) {
+    // catches cases where a full operation isn't inputted
     if (num1.length === 0 || num2.length === 0 || operator === null) {
         alert("Please input a proper operation: a number, an operator sign, and a number");
-    } else if (num2[0] === 0 && operator === "÷") {
-        alert("Oho, you're approaching me? Then come as close as you'd like! Dividing by 0 will never happen!");
-    } else {
-        // console.log(num1);
-        // console.log(num2);
-        // console.log(operator);
-
+        clearValues();
+    }
+    // handles logic for full operation inputs
+    else {
+        // store number parameters after conversion to integers
         digit1 = parseInt(num1.join(""));
         digit2 = parseInt(num2.join(""));
 
+        // call math operation function based on operator sign
         switch (operator) {
             case "+":
                 operationResult = add(digit1, digit2);
@@ -55,14 +56,26 @@ function operate(num1, num2, operator) {
                 break;
             case "÷":
                 operationResult = divide(digit1, digit2);
+                // catches instance of dividing by zero
+                if (operationResult === Infinity) {
+                    alert("Oho, you're approaching me? Then come as close as you'd like! Dividing by 0 will never happen!");
+                    clearValues();
+                }
                 break;
         }
-        return Number.isInteger(operationResult) ? operationResult : Number.parseFloat(operationResult.toFixed(2));
-        // console.log(operationResult);
+        // convert operation result to a rounded decimal if need be
+        if (Number.isInteger(operationResult)) {
+            return operationResult;
+        } else if (operationResult === null) {
+            return null;
+        } else {
+            return Number.parseFloat(operationResult.toFixed(2));
+        }
     }
 }
 
 function storeClickedValue(key) {
+    // handle chained operations
     if (operand1.length !== 0 && operand2.length !== 0 && key.classList.contains("operator-key")) {
         operand1 = [];
         operand2 = [];
@@ -70,6 +83,7 @@ function storeClickedValue(key) {
         operator = key.textContent;
     } else {
         if (key.classList.contains("number-key")) {
+            // handle inputs for operands, both single and multi-digit numbers, as well as which operand should store which input
             if (operator === null) {
                 operand1.push(key.textContent);
                 displayPanel.textContent = operand1.join("");
@@ -78,25 +92,37 @@ function storeClickedValue(key) {
                 displayPanel.textContent = operand2.join("");
             }
         } else if (key.classList.contains("operator-key")) {
+            // handle inputs for operator
             operator = key.textContent;
         }
     }
 }
 
+// clear operands and operator when called
 function clearValues() {
     operand1 = [];
     operand2 = [];
     operator = null;
+    operationResult = null;
     displayPanel.textContent = "";
 }
 
+// loop through all calculator keys and store their value when clicked
 for (const key of calcKeys) {
     key.addEventListener("click", () => {
         storeClickedValue(key);
     });
 }
 
+// clear operands and operator when clicked
 clearBtn.addEventListener("click", () => {clearValues()});
+
+// call operation function and display result in DOM
 equalSign.addEventListener("click", () => {
     displayPanel.textContent = operate(operand1, operand2, operator);
+
+    console.log(operand1);
+    console.log(operand2);
+    console.log(operator);
+    console.log(operationResult);
 })
